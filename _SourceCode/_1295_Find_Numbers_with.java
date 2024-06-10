176419bfdd62a6fe59865db2baba5ffcc2aa1770
Dennis Vo <dennisphuc@gmail.com>
2024-06-10 15:10:37 +0700

    feat: finish #1295 leetcode, first solution

--- _SourceCode/_1295_Find_Numbers_with.java
+++ _SourceCode/_1295_Find_Numbers_with.java
@@ -1,10 +1,32 @@
 public class _1295_Find_Numbers_with {
-    public int findNumbers(int[] nums) {
-        //TODO: implement the function here
-        return 0;
+    public static int findNumbers(int[] nums) {
+        int result = 0;
+        
+        for (int i : nums) {
+            if (hasEvenNumberOfDigits(i)) {
+                result++;
+            }
+        }
+
+        return result;
     }
     
+    private static boolean hasEvenNumberOfDigits(int i) {
+        int count = 0;
+        int result = i;
+
+        while (result != 0) {
+            result = i / 10;
+            i = result;
+            count++;
+        }
+
+        return count % 2 == 0;
+    }
+
     public static void main(String[] args) {
-        
+        int[] nums = {12,345,2,6,7896};
+
+        System.out.println("\n Output: " + findNumbers(nums));
     }
 }
