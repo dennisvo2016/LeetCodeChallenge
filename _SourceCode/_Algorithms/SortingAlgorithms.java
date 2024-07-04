349f23a145a0ee2449e4c1a288537932dedeffdc
Dennis Vo <dennisphuc@gmail.com>
2024-07-04 16:19:08 +0700

    fix: refactor folder structures + initiate the class for sorting algorithms

---
+++ _SourceCode/_Algorithms/SortingAlgorithms.java
@@ -0,0 +1,30 @@
+public class SortingAlgorithms {
+    public static void printArray(int no, int[] a)
+    {
+        System.out.printf("%d:", no);
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%d", a[i]);
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSort(int[] a)
+    {
+
+    }
+
+    public static void selectionSort(int[] a)
+    {
+        
+    }
+
+    public static void insertionSort(int[] a)
+    {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] a = {5, 3, 2, 7, 8, 1, 2}; //Test trường hợp thường
+        int[] b = {1, 2, 3, 4, 5, 7, 6}; //Test trường hợp nâng cấp
+    }
+}
