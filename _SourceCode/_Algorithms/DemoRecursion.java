58f65bb698eed9136e0ed4f4af78ba40ea6a2a6d
Dennis Vo <dennisphuc@gmail.com>
2024-10-25 11:14:20 +0200

    feat: some recursive functions
    - sum from 1 to n
    - exp N of A
    - print arr

--- _SourceCode/_Algorithms/DemoRecursion.java
+++ _SourceCode/_Algorithms/DemoRecursion.java
@@ -44,13 +44,58 @@ public class DemoRecursion {
         return fn[n];
     }
 
-    public static void main(String[] args) {
-        int n = 10;
-        System.out.println("factorial of " + n + " is: " + factorial(n));
-        System.out.println("factorial of " + n + " is: " + factorialNormal(n));
+    public static int sumFromOneToN(int n)
+    {
+        //T(n) = n + T(n-1)
+        if (n < 0) {
+            return -1; //invalid number
+        }
+        if (n == 0) {
+            return 0;
+        }
 
-        int nf = 3;
-        System.out.println("fibo at " + nf + " is: " + fibo(nf));
-        System.out.println("fibo at " + nf + " is: " + fiboNormal(nf));
+        return n + sumFromOneToN(n - 1);
+    }
+
+    public static int expNofA(int a, int n)
+    {
+        //A^(n) = A * A^(n-1)
+        if (n < 0) {
+            return -1; //invalid
+        }
+        if (n == 0) {
+            return 1;
+        }
+
+        return a * expNofA(a, n - 1);
+    }
+
+    public static void printArr(int[] arr, int index)
+    {
+        if (index >= arr.length) {
+            return;
+        }
+
+        System.out.print(arr[index] + " ");
+        printArr(arr, index + 1);
+    }
+
+    public static void main(String[] args) {
+        // int n = 10;
+        // System.out.println("factorial of " + n + " is: " + factorial(n));
+        // System.out.println("factorial of " + n + " is: " + factorialNormal(n));
+
+        // int nf = 3;
+        // System.out.println("fibo at " + nf + " is: " + fibo(nf));
+        // System.out.println("fibo at " + nf + " is: " + fiboNormal(nf));
+
+        // int[] arr = {0, 9, 4, 6, 3, 6, 6, 9, 5, 8};
+        // printArr(arr, 0);
+
+        // int n = -5;
+        // System.out.println("sum from 1 to " + n + " is: " + sumFromOneToN(n));
+
+        int a = 4, n1 = -10;
+        System.out.println("exp " + n1 + " of " + a + " is: " + expNofA(a, n1));
     }
 }
